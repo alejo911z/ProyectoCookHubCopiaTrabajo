c6cfb75d935e3bc252652e9c16b323658bcf3d89
alejo911z <y.sanchez6@uniandes.edu.co>
2026-09-06 21:16:01 -0500

    Importar db y SQLALCHEMY_DATABASE_URI

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
-import os
-
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from modelos import db
 from vistas import (
     VistaRecetas,
     VistaReceta,
@@ -14,15 +11,7 @@ from vistas import (
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "cookhub-secret-key"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "COOKHUB_DATABASE_URL", "sqlite:///cookhub.db"
-)
 CORS(app)
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 api = Api(app)
 
